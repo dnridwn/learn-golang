2ccc6e266c357d512e171b9e1592ae68b3c40669
dnridwn <denridwan123@gmail.com>
2023-06-04 01:53:59 +0700

    boolean operator

---
+++ boolean-operator.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilaiAkhir = 90
+	var absensi = 75
+
+	// var lulusNilaiAkhir bool = nilaiAkhir >= 80
+	// var lulusAbsensi bool = absensi >= 80
+	// fmt.Println(lulusNilaiAkhir)
+	// fmt.Println(lulusAbsensi)
+
+	// var lulus bool = lulusNilaiAkhir && lulusAbsensi
+
+	// fmt.Println(lulus)
+
+	fmt.Println(nilaiAkhir >= 80 && absensi >= 80)
+}

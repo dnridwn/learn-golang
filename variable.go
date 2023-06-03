a185d0a625c9b5dbda82423bf43cf834535ea797
dnridwn <denridwan123@gmail.com>
2023-06-03 20:51:18 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Den Ridwan"
+	fmt.Println(name)
+
+	name = "Den Saputra"
+	fmt.Println(name)
+
+	var friendName = "Budi"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Den Ridwan"
+		lastName  = "Saputra"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}

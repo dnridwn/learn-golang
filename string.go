b44a20f49266d4df4f732bc967ea4bc3a8017c8a
dnridwn <denridwan123@gmail.com>
2023-06-03 20:36:06 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Den")
+	fmt.Println("Den Ridwan")
+	fmt.Println("Den Ridwan Saputra")
+
+	fmt.Println(len("Den"))
+	fmt.Println("Den Ridwan"[0])
+	fmt.Println("Den Ridwan Saputra"[1])
+}

f64e60e632d3acf2e35c5c4a1853b9423b5ab779
dnridwn <denridwan123@gmail.com>
2023-06-07 00:33:18 +0700

    blank identifier

---
+++ blank-identifier.go
@@ -0,0 +1,7 @@
+package main
+
+import _ "learn-golang/database" // import dengan menambahkan _ di depan, sehingga golang tidak akan komplain walaupun package di import tapi tidak di pakai
+
+func main() {
+
+}
